c6577f049eea2045f804f60b51614c34bf4cb325
Yilen <yilen.pan@gmail.com>
2015-12-01 18:27:26 -0800

    Added static route and travis

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
 module.exports = function (app, express) {
+  app.use(express.static(__dirname + '../../client/'));
   app.get('/', function (req, res) {
-    res.send('OK');
+    res.sendStatus(200);
   });
 };
