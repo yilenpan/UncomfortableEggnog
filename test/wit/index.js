0a97d96216dc6f5969068e502aeb402a34c31b3e
Andres <andres.mm07@gmail.com>
2015-12-02 19:55:05 -0800

    working implementation of wit.ai

--- test/wit/index.js
+++ test/wit/index.js
@@ -2,32 +2,30 @@ var wit = require('node-wit');
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var ACCESS_TOKEN = "Y5TWYIS2VCANGWNE765LXGBLR3Y226YT";
 var http = require('http');
-var ip = "127.0.0.1";
-var port = 3000;
+var ACCESS_TOKEN = 'Y5TWYIS2VCANGWNE765LXGBLR3Y226YT';
 
 var app = express();
-app.use(bodyParser.json({limit: "50mb"}));
+// app.use(bodyParser.json());
 
-console.log("Sending text & audio to Wit.AI");
+console.log("Sending text & audio to Wit.AI!");
 
-wit.captureTextIntent(ACCESS_TOKEN, "Hello world", function (err, res) {
-    console.log("Response from Wit for text input: ");
-    if (err) console.log("Error: ", err);
-    console.log(JSON.stringify(res, null, " "));
-});
+app.use(express.static('client'));
 
-var stream = fs.createReadStream('../assets/audio/voicetests-examplerun.wav');
-wit.captureSpeechIntent(ACCESS_TOKEN, stream, "audio/wav", function (err, res) {
-    console.log("Response from Wit for audio stream: ");
-    if (err) console.log("Error: ", err);
-    console.log(JSON.stringify(res, null, " "));
-});
+// app.get('/', function(req, res) {
+//   // wit.captureTextIntent(ACCESS_TOKEN, "Hello world", function (err, res) {
+//   //     console.log("Response from Wit for text input: ");
+//   //     if (err) console.log("Error: ", err);
+//   //     console.log(JSON.stringify(res, null, " "));
+//   // });
 
-// var server = http.createServer(function(request, response) {
-
-
+//   // var stream = fs.createReadStream('../assets/audio/myRecording01.wav');
+//   // wit.captureSpeechIntent(ACCESS_TOKEN, stream, "audio/wav", function (err, result) {
+//   //     console.log("Response from Wit for audio stream: ");
+//   //     if (err) console.log("Error: ", err);
+//   //     console.log(JSON.stringify(result, null, " "));
+//   //     res.status(200).json(result._text);
+//   // });
 // });
 
-//server.listen(port, ip);
+app.listen(3000);
