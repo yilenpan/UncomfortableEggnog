fabd0a328bf5a31989c40b7a37cdef8e38ca2644
Mitchell Wilcox <mlwilcox@gmail.com>
2015-12-04 12:00:40 -0800

    (core) fill in some helper and controller functions.

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -41,9 +41,10 @@ exports.signupUser = function (req, res) {
 //~~~~~~~~~~~~refactor this to helpers~~~~~~~~~
 exports.getUserInfo = function (req, res) {
   var id = req.params.id;
-  db.User.findOne( { id: id }, function (err, user) {
+  helpers.findUser(id, function (err, user) {
     if (err) {
       console.log(err);
+      res.sendStatus(500);
     }
     console.log(user);
     res.send(user);
@@ -63,14 +64,43 @@ exports.checkUser = function (req, res, next) {
 **************************************/
 
 exports.fetchPackages = function (req, res) {
-
+  helpers.findPackageEntries({}, function (err, packages) {
+    if (err) {
+      console.log('There was an error finding packages.');
+      res.sendStatus(500);
+    } else {
+      console.log('Sending packages to client.');
+      res.send(packages);
+    }
+  });
 };
 
 exports.fetchPackage = function (req, res) {
-
+  var id = req.params.id;
+  helpers.findPackageEntries( {id: id}, function (err, packageEntry) {
+    if (err) {
+      console.log('There was an error finding package with ID: ' + id + '.');
+      res.sendStatus(500);
+    } else if (packageEntry.length === 0) {
+        console.log('No entry found with ID: ' + id);
+        res.sendStatus(404);
+    } else {
+       console.log('Sending package with ID ' + id + ' to client.');
+       res.send(packages);
+    }
+  });
 };
 
 exports.savePackageEntry = function (req, res) {
-
+  //entry should be object with all relevant PackageEntry attributes
+  var entry = req.body;
+  helpers.savePackage(entry, function (err, packageEntry) {
+    if (err) {
+      console.log('There was an error saving package.');
+      res.sendStatus(500);
+    } else {
+      console.log('Saved entry to the database!');
+      res.send(packageEntry);
+    }
+  });
 };
-
--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -1,6 +1,7 @@
 //helpers are a set of functions that work with the database.
 var bcrypt = require('bcrypt-nodejs');
 var bluebird = require('bluebird');
+var db = require('../db/db.js');
 
 var SALT_WORK_FACTOR = 10;
 
@@ -14,24 +15,52 @@ exports.comparePassword = function (candidatePassword, cb) {
 };
 
 exports.hashPassword = function (next) {
-  var user = this;
-// only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) {
-    return next();
-  }
-// generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-    if (err) {
-      return next(err);
-    }
-// hash the password along with our new salt
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      if (err) {
-        return next(err);
-      }
-// override the cleartext password with the hashed one
-      user.password = hash;
+  var cipher = bluebird.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function (hash) {
+      this.password = hash;
       next();
     });
-  });
 };
+
+exports.findPackageEntries = function (query, cb) {
+  db.PackageEntry.find(query, cb);
+};
+
+
+
+exports.savePackage = function (entry, cb) {
+  var packageEntry = new db.PackageEntry(entry);
+  packageEntry.save(cb);
+};
+
+exports.findUser = function (id, cb) {
+  db.User.findOne( { id: id }, cb);
+};
+
+// exports.findPackage = function (id, cb) {
+//   db.PackageEntry.find({id: id}, cb);
+// }
+
+// exports.hashPassword = function (next) {
+//   var user = this;
+// // only hash the password if it has been modified (or is new)
+//   if (!user.isModified('password')) {
+//     return next();
+//   }
+// // generate a salt
+//   bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+//     if (err) {
+//       return next(err);
+//     }
+// // hash the password along with our new salt
+//     bcrypt.hash(user.password, salt, function (err, hash) {
+//       if (err) {
+//         return next(err);
+//       }
+// // override the cleartext password with the hashed one
+//       user.password = hash;
+//       next();
+//     });
+//   });
+// };
