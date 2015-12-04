4f631d58fb82581e61cf0b1ea250c871c78d941f
Yilen <yilen.pan@gmail.com>
2015-12-03 16:02:34 -0800

    added routes

--- client/js/app.config.js
+++ client/js/app.config.js
@@ -4,9 +4,36 @@
   angular.module('app')
     .config(config);
 
-  config.$inject = [];
-  function config() {
-
+  config.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function config($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('main', {
+        url: '/',
+        views: {
+          top: {
+            templateUrl: 'js/html/landing/landing.top.html',
+            controller: 'NavCtrl'
+          },
+          main: {
+            templateUrl: 'js/html/landing/landing.main.html',
+            controller: 'MainPageCtrl'
+          }
+        }
+      })
+      .state('package', {
+        url: '/package/:packageName',
+        views: {
+          top: {
+            templateUrl: 'js/html/nav/navbar.top.html',
+            controller: 'NavCtrl'
+          },
+          main: {
+            templateUrl: 'js/html/package/package.main.html',
+            controller: 'PackageCtrl'
+          }
+        }
+      });
   }
 
 })();
