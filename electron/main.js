08769f9b9d96be45c094aa828dfde6f1998d4687
Andres <andres.mm07@gmail.com>
2015-12-04 10:53:51 -0800

    fixed typo with unregistering shortcut

--- electron/main.js
+++ electron/main.js
@@ -50,6 +50,7 @@ app.on('ready', function () {
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
+    globalShortcut.unregister('ctrl+r');
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
