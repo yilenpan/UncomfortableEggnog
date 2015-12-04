2dd4a8a09ae7139971a7a9c2cccad07675fb9e63
Tracy Duong <tpduong1@gmail.com>
2015-12-03 19:44:45 -0800

    rebase

---
+++ client/js/controllers/landing/landing.controller.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+  angular.module('app')
+    .controller('LandingPageCtrl', LandingPageCtrl);
+
+  LandingPageCtrl.$inject = [];
+
+  function LandingPageCtrl () {
+    var self = this;
+
+  }
+})();
