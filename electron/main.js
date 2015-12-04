d349d96199f4d61c3475c19167b9b34aab563590
Andres <andres.mm07@gmail.com>
2015-12-04 10:48:05 -0800

    last changes before pull request with global shortcut

--- electron/main.js
+++ electron/main.js
@@ -21,7 +21,7 @@ electron.crashReporter.start();
 var mainWindow = null;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform != 'darwin') {
@@ -31,9 +31,12 @@ app.on('window-all-closed', function() {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
+app.on('ready', function () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
@@ -41,12 +44,12 @@ app.on('ready', function() {
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
-  var startRecording = globalShortcut.register('ctrl+r', function(){
+  var startRecording = globalShortcut.register('ctrl+r', function () {
     mainWindow.webContents.send('startRecord', 'rec!');
   });
-  
+
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
