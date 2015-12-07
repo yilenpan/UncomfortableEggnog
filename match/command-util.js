865b87d1eedf14af28f649b9b659643776545fb0
Tracy Duong <tpduong1@gmail.com>
2015-12-07 12:22:00 -0800

    command-util can handle close match confirmation

--- match/command-util.js
+++ match/command-util.js
@@ -15,59 +15,73 @@ var regMatch = function (arr, term) {
   });
 };
 
+var addPhrase = function (newPhraseObj) {
+  //read from phrasesPath
+  var phrases = JSON.parse(fs.readFileSync(newPhraseObj.phrasesPath, 'utf8'));
+
+  //push in new phrase
+  phrases[newPhraseObj.phrase].push(newPhraseObj.inputphrase);
+
+  // write back to phrases
+  fs.writeFileSync(newPhraseObj.phrasesPath, JSON.stringify(phrases), 'utf8');
+  console.log("saving ", newPhraseObj.inputphrase, "as acceptable phrase for \'", newPhraseObj.phrase, "\'");
+};
+
 var matching = function (input, phrasesPath) {
+  var commandObj = {
+    command: "",
+    exact: true,
+    phrase: "",
+    inputphrase: input.term,
+    phrasesPath: phrasesPath
+  };
   var phrases = JSON.parse(fs.readFileSync(phrasesPath, 'utf8'));
+
 
   for (var key in phrases) {
     if (regMatch(phrases[key], input.term)) {
       console.log("term is: ", input.term, " exact match found, phrase is :", key);
-      return key;
+      commandObj.phrase = key;
+      return commandObj;
     }
 
     if (natural.JaroWinklerDistance(input.term, key) > 0.8) {
-      // console.log("do you mean: ", key, "? (y/n)");
-      // console.log("assuming user said yes for testing purposes... ");
-
-      phrases[key].push(input.term);
-
-      // when add new phrases
-      fs.writeFileSync(phrasesPath, JSON.stringify(phrases), 'utf8');
-
-      console.log("saving ", input.term, "as acceptable phrase for \'", key, "\'");
-
-      console.log("term is: ", input.term, " close match found, phrase is :", key);
-      return key;
+      console.log("term is: ", input.term, " possible close match found, phrase is :", key);
+      commandObj.exact = false;
+      commandObj.phrase = key;
+      return commandObj;
     }
 
     if (natural.JaroWinklerDistance(metaphone.process(input.term), metaphone.process(key)) > 0.8) {
-      // console.log("do you mean: ", key, "? (y/n)");
-      // console.log("assuming user said yes for testing purposes... ");
-
-      phrases[key].push(input.term);
-      fs.writeFileSync(phrasesPath, JSON.stringify(phrases), 'utf8');
-      console.log("saving ", input.term, "as acceptable phrase for \'", key, "\'");
-
-      console.log("term is: ", input.term, " phonetic match found, phrase is :", key);
-      return key;
+      console.log("term is: ", input.term, " possible phonetic match found, phrase is :", key);
+      commandObj.exact = false;
+      commandObj.phrase = key;
+      return commandObj;
     }
   }
 
   console.log("term is: ", input.term, " no match");
-  return null;
+  commandObj.exact = false;
+  commandObj.phrase = null;
+  return commandObj;
 };
 
 var commandUtil = function (input, fileInfo) {
-  var phrase = matching(input, fileInfo.phrasesPath);
-  return fileInfo.commands[phrase];
+  var commandObj = matching(input, fileInfo.phrasesPath);
+  var phrase = commandObj.phrase;
+  //add bash shell
+  commandObj.command = fileInfo.commands[phrase];
+  return commandObj;
 };
 
 //for testing
 // var commands = JSON.parse(fs.readFileSync('./commands.json', 'utf8'));
 // var testObject = JSON.parse(fs.readFileSync('/Users/tpduong/src/hack-reactor/UncomfortableEggnog/test/assets/parse-test.json', 'utf8'));
 
-// for (var i = 0; i < testObject.length; i++) {
-//   var command = commandUtil(testObject[i], {commands: commands, phrasesPath: './phrases.json'});
-//   console.log(command);
+// // for (var i = 0; i < testObject.length; i++) {
+//   var command = commandUtil(testObject[1], {commands: commands, phrasesPath: './phrases.json'});
+//   // console.log(command);
+//  addPhrase(command);
 // }
 
 module.exports.commandUtil = commandUtil;
