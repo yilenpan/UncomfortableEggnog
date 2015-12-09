88cd5e935eca8a57b4cfa3421297b29214cf73bf
Yilen <yilen.pan@gmail.com>
2015-12-09 15:13:03 -0800

    testing sanitizer

--- electron/cmd/execShellCommand.js
+++ electron/cmd/execShellCommand.js
@@ -1,6 +1,10 @@
 var exec = require('child_process').exec;
+var santize = function (str) {
+  return str.replace(/[^a-z0-9\s]/gi, '');
+};
+// sanitze rips out any char not a letter or number or white space
 module.exports = function (shellCommand) {
-  exec(shellCommand, function (error, stdout, stderr) {
+  exec(sanitize(shellCommand), function (error, stdout, stderr) {
     console.log("executed shell command");
     console.log('Stoping commandRecognition');
     if (error) {
