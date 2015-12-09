e3d911bf8be3ca1cb41b64e2d74bfc094d66df51
Mitchell Wilcox <mlwilcox@gmail.com>
2015-12-08 22:18:51 -0800

    (feat) completed sign up form functionality and displays.

--- client/js/controllers/signup/signup.controller.js
+++ client/js/controllers/signup/signup.controller.js
@@ -7,12 +7,43 @@
 
   function SignUpCtrl (ApiFactory, $state, $scope, SignUpFactory) {
     var self = this;
-    self.user = {};
-    self.passwordStrengthNum = 0;
-    // self.passwordColor = 'white';
+
+    // self.fields.password.strength = 0;
+
+    self.fields = {
+      "email": {
+        required: true,
+        value: ''
+      },
+      "website": {
+        required: false,
+        value: ''
+      },
+      "first name": {
+        required: true,
+        value: ''
+      },
+      "last name": {
+        required: true,
+        value: ''
+      },
+      "username": {
+        required: true,
+        value: ''
+      },
+      "password": {
+        required: true,
+        value: '',
+        strength: 0
+      },
+      "password repeat": {
+        required: false,
+        value: ''
+      }
+    };
 
     self.errorMessages = {
-      URL: 'Please enter a valid URL (Did you prefix your url with http://?).',
+      website: 'Please enter a valid URL (Did you prefix your website with http://?).',
       email: 'Invalid email address.',
       minlength: 'Username is too short (4 to 12 characters allowed)',
       maxlength: 'Username is too long (4 to 12 characters allowed)',
@@ -21,30 +52,25 @@
     };
 
     self.isPassStrengthStrong = function () {
-      return self.passwordStrengthNum > 70;
+      return self.fields.password.strength > 70;
     };
     self.isPassStrengthGood = function () {
-      return self.passwordStrengthNum > 30 && self.passwordStrengthNum <= 70;
+      return self.fields.password.strength > 30 && self.fields.password.strength <= 70;
     };
     self.isPassStrengthWeak = function () {
-      return self.passwordStrengthNum <= 30;
+      return self.fields.password.strength <= 30;
     };
-    // {
-    //   weak: 'red',
-    //   good: 'yellow',
-    //   strong: 'green'
-    // };
+
     self.post = function () {
-      console.log(self.user);
       ApiFactory.post('/signup', {
-        username: self.username,
-        password: self.password
+        username: self.fields.username.value,
+        password: self.fields.password.value
       }).then(function (result) {
         if (result.error) {
           self.errorList = [result.error];
 // =======
-          self.username = '';
-          self.password = '';
+          self.fields.username.value = '';
+          self.fields.password.value = '';
         // if (result.errorType === 'username') {
           // console.log(result.error);
           // self.usernameError = result.error;
@@ -64,38 +90,45 @@
       });
     };
 
-    $scope.$watch('su.password', function (pass) {
+    $scope.$watch('su.fields.password.value', function (pass) {
       var strength = SignUpFactory.getPasswordStrength(pass);
-      self.passwordStrengthNum = strength;
-      // console.log(pass);
-      // self.passStrength = SignUpFactory.checkPasswordStrength(strength);
+      self.fields.password.strength = strength;
     });
 
 
     self.validateAndPost = function () {
+      var validated = true;
       self.errorList = [];
-        $('.required').each(function (field, elem) {
-          if ($scope.signupForm[elem.name].$dirty === false) {
-            self.errorList.push('Please enter your ' + elem.name + '.');
-            $(this).parent().addClass('has-error');
-          } else if ($scope.signupForm[elem.name].$invalid) {
-            $(this).parent().addClass('has-error');
-          } else {
-            $(this).parent().addClass('has-success');
+      $scope.signupForm.$setDirty(true);
+          for (var key in self.fields) {
+            var field = self.fields[key];
+            if (field.required) {
+              console.log(field.value);
+              if (!field.value) {
+            $scope.signupForm[key].$dirty = true;
+                self.errorList.push('Please enter your ' + key + '.');
+                // self.field[]
+                validated = false;
+              }
+            }
           }
-        });
-      //check password match
-
-      if (self.password !== self.passwordRepeat) {
-        self.password = '';
-        self.passwordRepeat = '';
-        self.errorList.push(self.errorMessages[password]);
-        $('input[type=password]').parent().addClass('has-error');
+      if (validated) {
+        self.post();
       }
     };
 
-    self.isInputValid = function (input) {
+    self.isInputInvalid = function (input) {
       return input.$dirty && input.$invalid;
+    };
+
+    self.isInputValid = function (input) {
+      return input.$dirty && input.$valid && !(input.$error.minlength);
+    };
+
+    self.setPristine = function (field) {
+      if (field.$$lastCommittedViewValue === '') {
+        field.$setPristine();
+      }
     };
 
   }
