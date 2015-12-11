8fa3fba44cfa94e51ac59edce1ff37ff0bc472e7
Andres <andres.mm07@gmail.com>
2015-12-10 16:29:33 -0800

    stars rating directive displays and users are able to interact with it

--- client/js/directives/rating/rating.directive.js
+++ client/js/directives/rating/rating.directive.js
@@ -1,19 +1,57 @@
 (function () {
   'use strict';
   angular.module('app')
-    .directive('rating', cards);
+    .directive('rating', RatingDirective);
 
   function RatingDirective () {
     var directive = {
       restrict: 'AE',
-      templateUrl: 'js/html/nav/navbar.top.html',
-      link: link
+      templateUrl: 'js/html/rating/rating.main.html',
+      link: link,
+      scope: {
+        score: '=score',
+        max: '=max'
+      }
     };
 
     return directive;
 
     function link(scope, elem, attrs) {
-      console.log('ratings');
+      scope.updateStars = function () {
+        var index = 0;
+        scope.stars = [];
+        for (index = 0; index < scope.max; index++) {
+          scope.stars.push({
+            full: scope.score > index
+          });
+        }
+      };
+      scope.starClass = function (star, index) {
+        var starClass = 'fa-star-o';
+        if (star.full) {
+          starClass = 'fa-star';
+        }
+
+        return starClass;
+      };
+
+      scope.$watch('score', function (newValue, oldValue) {
+        if (newValue !== null && newValue !== undefined) {
+          scope.updateStars();
+        }
+      });
+
+      scope.setRating = function (index) {
+        scope.score = index + 1;
+      };
+
+      scope.hover = function (index) {
+        scope.hoverIndex = index;
+      };
+
+      scope.stopHover = function () {
+        scope.hoverIndex = -1;
+      };
     }
   }
 })();
