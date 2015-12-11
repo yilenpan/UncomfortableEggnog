a591c34b7c8c097aa6f79543a1b09035bc853310
Mitchell Wilcox <mlwilcox@gmail.com>
2015-12-10 19:34:57 -0800

    changed location of folder

--- match/regMatch.js
+++
@@ -1,7 +0,0 @@
-var _ = require('underscore');
-module.exports = function (arr, term) {
-  var regTerm = new RegExp(term, 'i'); // creates new regexp obj
-  return _.some(arr, function (key) {
-    return key.match(regTerm); // tests key against regTerm
-  });
-};
