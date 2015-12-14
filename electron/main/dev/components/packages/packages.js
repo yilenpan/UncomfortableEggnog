4378e7a18e29878e1a2af876afcafecddb695f4e
Andres <andres.mm07@gmail.com>
2015-12-14 12:26:02 -0800

    started playing with views

--- electron/main/dev/components/packages/packages.js
+++ electron/main/dev/components/packages/packages.js
@@ -7,14 +7,23 @@ export default class Packages extends React.Component {
   render() {
     let saying = "Packages";
     return (
-      <div onClick={(e) => {
-        ipcRenderer.send('async message', saying);
-        ipcRenderer.on('async reply', (event, arg) => {
-          console.log('got something!!!!');
-          console.log(arg);
-        });
-      }}>
-        {saying}
-      </div>);
+      // <div onClick={(e) => {
+      //   ipcRenderer.send('async message', saying);
+      //   ipcRenderer.on('async reply', (event, arg) => {
+      //     console.log('got something!!!!');
+      //     console.log(arg);
+      //   });
+      // }}>
+      //   {saying}
+      // </div>
+      <div className="row">
+        <div className="col-md-4">
+          Options
+        </div>
+        <div className="col-md-12">
+          List of Packages
+        </div>
+      </div>
+      );
   }
 };
