a0a9d100d1762c293fa38fae7f5ad22fed7d64e4
Yilen <yilen.pan@gmail.com>
2015-12-14 20:49:21 -0800

    more improvements for prefixRec

--- electron/listener/prefixRec.js
+++ electron/listener/prefixRec.js
@@ -4,12 +4,17 @@ var phoneticsTest = require('../match/testers/phoneticsTest');
 var regMatch = require('../match/regMatch');
 
 module.exports = function (event) {
+  var word = '';
   for (var i = event.resultIndex; i < event.results.length; ++i) {
-    var word = event.results[i][0].transcript;
-    if (phoneticsTest(word, 'Jarvis') > 0.8 || regMatch(word.split(' '), 'Jarvis')) {
-      this.switchListener.hasTimeout = true;
-      startCmd.play();
-      this.switch();
-    }
+    var transcript = event.results[i][0].transcript;
+    var words = transcript.split('\n');
+    console.log(words);
+    var word = words[words.length - 1];
+  }
+  console.log(word);
+  if (phoneticsTest(word, 'Jarvis') > 0.8) {
+    this.switchListener.hasTimeout = true;
+    startCmd.play();
+    this.switch();
   }
 };
--- electron/main/speechRecognition.js
+++ electron/main/speechRecognition.js
@@ -10,8 +10,20 @@ if (!('webkitSpeechRecognition' in window)) {
 } else {
   //instance that will listen for the prefix
   var prefixRecognition = listener(prefixRec, 'prefix');
-  prefixRecognition.continuous = true;
+  // prefixRecognition.continuous = true;
   prefixRecognition.interimResults = true;
+  prefixRecognition.onaudioend = function () {
+    console.log('audio ended, restarting');
+    this.stop();
+  };
+  // prefixRecognition.onsoundend = function () {
+  //   console.log('sound ended, restarting');
+  //   this.stop();
+  // };
+  // prefixRecognition.onspeechend = function () {
+  //   console.log('speech ended, restarting');
+  //   this.stop();
+  // };
   //instance that will listen for the command
   var commandRecognition = listener(cmdRec, 'cmd', 5000);
   // connect the two so that tdhe prefixRec will stop its process and kick off
