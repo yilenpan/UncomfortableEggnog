48f392f679c21f4852ea954148910a5a1a020952
Andres <andres.mm07@gmail.com>
2015-12-16 11:08:38 -0800

    saving commandsUtil

--- electron/main/dist/bundle.js
+++ electron/main/dist/bundle.js
@@ -24618,8 +24618,6 @@
 	};
 
 	module.exports.addCommand = function (command) {
-	  console.log('I GOT A COMMAND FROM THE VIEWS');
-	  console.log(command);
 	  var newCommandsObj = _.extend({}, this.getCommands());
 	  newCommandsObj.rawCommands = lowerCaseProps(_.extend(this.getCommands().rawCommands, command));
 	  newCommandsObj.parsedCommands = parseCommands(newCommandsObj.rawCommands);
