7eaf7092709052a64ac69c3a725df4d79d3c18e4
Yilen <yilen.pan@gmail.com>
2015-12-28 17:31:43 -0800

    todo

--- electron/main/dev/components/settings/settings.js
+++ electron/main/dev/components/settings/settings.js
@@ -1,26 +1,24 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import openBrowser from '../../../../cmd/execShellCommand';
 import AppActions from '../../actions/actions';
 
 
 export default class Settings extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: ""
-    };
-  }
-
   render() {
     return (
       <div>
         <div className="input-group">
           <input type="text" className="form-control" placeholder="Jarvis" ref="newName"/>
           <span className="input-group-btn">
-            <button className="btn btn-success" type="button" onClick={e => {
-              console.log(this.refs.newName.value);
-              AppActions.changeConfig(this.refs.newName.value);
-            }}>Change Name</button>
+            <button
+              className="btn btn-success"
+              type="button"
+              onClick={e => {
+                //TODO: if we want to chane anything else in the config, we do it here
+                AppActions.changeConfig(this.refs.newName.value);
+              }}
+            >
+              Change Name</button>
           </span>
         </div>
         <div>
