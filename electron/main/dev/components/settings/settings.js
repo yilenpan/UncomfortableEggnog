23d8802262aec9e824a0f8563e46ac79b44e29d0
Yilen <yilen.pan@gmail.com>
2015-12-29 11:35:34 -0800

    settings styles

--- electron/main/dev/components/settings/settings.js
+++ electron/main/dev/components/settings/settings.js
@@ -13,59 +13,71 @@ function getConfig () {
 const Settings = (props) => {
   let {name, exactMatchThreshold, closeMatchThreshold} = props.config;
   return (
-    <div className="row">
-      <div className="col-md-12">
-        <label> Name </label>
-        <input
-          type="text"
-          className="form-control"
-          value={name}
-          onChange={ e => {
-            name = e.target.value;
-            AppActions.changeConfig({
-              name
-            });
-          }}
-        />
-        <span>
-          <label> exactMatchThreshold </label>
-          <input
-            type="range"
-            step="0.01"
-            max="1"
-            min="0"
-            value={exactMatchThreshold}
-            onChange={ e => {
-              exactMatchThreshold = e.target.value;
-              AppActions.changeConfig({
-                exactMatchThreshold
-              });
-            }}
-
-          />
-          {exactMatchThreshold}
-        </span>
-        <span>
-          <label> closeMatchThreshold </label>
-          <input
-            type="range"
-            step="0.01"
-            max="1"
-            min="0"
-            value={closeMatchThreshold}
-            onChange={ e => {
-              closeMatchThreshold = e.target.value;
-              AppActions.changeConfig({
-                closeMatchThreshold
-              });
-            }}
-
-          />
-          {closeMatchThreshold}
-        </span>
-
+    <div className="text-center settings">
+      <h1> Settings </h1>
+      <div className="col-md-8 setting-input col-md-offset-2 text-left">
+        <div className="row">
+          <div className="col-md-4">
+            <label> Call Name </label>
+          </div>
+          <div className="col-md-8">
+            <input
+              type="text"
+              className="form-control"
+              value={name}
+              onChange={ e => {
+                name = e.target.value;
+                AppActions.changeConfig({
+                  name
+                });
+              }}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <label> Exact Matching Threshold </label>
+          </div>
+          <div className="col-md-8">
+            <input
+              type="range"
+              step="0.01"
+              max="1"
+              min="0"
+              value={exactMatchThreshold}
+              onChange={ e => {
+                exactMatchThreshold = e.target.value;
+                AppActions.changeConfig({
+                  exactMatchThreshold
+                });
+              }}
+            />
+            <p className="pull-right">{exactMatchThreshold}</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <label> Close Matching Threshold </label>
+          </div>
+          <div className="col-md-8">
+            <input
+              type="range"
+              step="0.01"
+              max="1"
+              min="0"
+              value={closeMatchThreshold}
+              onChange={ e => {
+                closeMatchThreshold = e.target.value;
+                AppActions.changeConfig({
+                  closeMatchThreshold
+                });
+              }}
+            />
+            <p className="pull-right">{closeMatchThreshold}</p>
+          </div>
+        </div>
         <button
-          className="btn btn-success"
+          className="btn btn-success pull-right"
           onClick={ e => {
             AppActions.saveConfig();
           }}
