faab7b8e5a61004f6ec2e2ccf68c291827656683
Andres <andres.mm07@gmail.com>
2015-12-29 19:56:38 -0800

    saving changes

--- electron/main/speechRecognition.js
+++ electron/main/speechRecognition.js
@@ -22,11 +22,8 @@ if (!('webkitSpeechRecognition' in window)) {
 ipcRenderer.on('listening', function (event) {
   console.log("Jarvis is listening!");
   var commandsUtil = require('./commandsUtil/commandsUtil');
-  var config = require('./config/config');
   require('./config/configUtils').get(function (err, data) {
     // load name into localStorage
-    console.log('data1: ', JSON.parse(data));
-    console.log('data2: ', config);
     localStorage.setItem('name', 'jarvis');
     commandsUtil.loadPackage(JSON.parse(data), function (err, data) {
       prefixRecognition.start();
